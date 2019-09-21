e65487b09d4ebdc915e80a81878db34280ee1900
Alexey_Lisenko <detoner777@gmail.com>
2019-09-21 19:54:44 +0300

    FileUpload component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
-
+import FileUpload from "./components/FileUpload";
 import "./App.css";
 
-function App() {
-  return <div className="App">app</div>;
-}
+const App = () => (
+  <div className="container mt-4">
+    <h4 className="display-4 text-center mb-4">
+      <i className="fab fa-react" style={{ color: "black" }} /> React File
+      Upload
+    </h4>
+
+    <FileUpload />
+  </div>
+);
 
 export default App;
