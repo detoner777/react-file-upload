9daf306b03d03015a2cd5ea9dbfcf93adfc941c0
Alexey_Lisenko <detoner777@gmail.com>
2019-09-23 10:00:46 +0300

    bug fix with react logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import FileUpload from "./components/FileUpload";
 import "./App.css";
+import Img from "./react.png";
 
 const App = () => (
   <div className="container mt-4">
     <h4 className="display-4 text-center mb-4">
-      <i className="fab fa-react" style={{ color: "black" }} /> React File
+      <img src={Img} alt="react" style={{ width: "7rem" }}></img> React File
       Upload
     </h4>
 
